933624412c71652cd960c4abfde0295f09cc4491
Liang Liu <nlprliu@gmail.com>
2016-05-28 22:33:12 -0400

    Only increase bundle version automatically for release build.

--- Scripts/increaseBundleVersion.sh
+++ Scripts/increaseBundleVersion.sh
@@ -1,11 +1,11 @@
+set -ex
+
 plist_file=$SRCROOT/MacKey/Info.plist
 short_version=$( /usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "$plist_file" )
-echo $short_version
-build_number=1
-build_number_file="$SRCROOT/build_number"
-[ -e "$build_number_file" ] && build_number=$( cat "$build_number_file" )
-new_build_number=$((build_number+1))
-bundleID="$short_version.$new_build_number"
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $bundleID" "$plist_file"
+old_bundle_version=$( /usr/libexec/PlistBuddy -c "Print :CFBundleVersion" "$plist_file" )
 
-echo $new_build_number > "$build_number_file"
+build_number=1
+[ -z "$old_bundle_version" ] || build_number=$( echo "$old_bundle_version" | grep -Eo "[^.]*$" )
+new_build_number=$((build_number+1))
+new_bundle_version="$short_version.$new_build_number"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $new_bundle_version" "$plist_file"
