4e1097f84eb7121a72351113bb5090d6bbd4a251
Liang Liu <nlprliu@gmail.com>
2017-01-28 19:30:10 -0500

    Refactor by adding HostDetailsViewMode.

--- MacKey/ViewControllers/HostDetailsViewController.swift
+++ MacKey/ViewControllers/HostDetailsViewController.swift
@@ -27,35 +27,24 @@ class HostDetailsViewController: UITableViewController {
         tableView.tableFooterView = UIView()
         tableView.allowsSelection = false
         passwordOutlet.isSecureTextEntry = true
-        populateHostInfo()
+        
+        let viewModel = HostDetailsViewModel(
+            input: (
+                alias: aliasOutlet.rx.text.orEmpty.asDriver(),
+                host: hostOutlet.rx.text.orEmpty.asDriver(),
+                username: usernameOutlet.rx.text.orEmpty.asDriver(),
+                password: passwordOutlet.rx.text.orEmpty.asDriver()
+            )
+        )
+        aliasOutlet?.text = viewModel.initialValues.alias
+        hostOutlet?.text = viewModel.initialValues.host
+        usernameOutlet?.text = viewModel.initialValues.username
+        passwordOutlet?.text = viewModel.initialValues.password
         
         validationOutlet.text = "Alias is already taken."
         
-        let aliasValid = aliasOutlet.rx.text.orEmpty.asDriver()
-            .map { [weak self] in $0.characters.count > 0 && self?.isAliasAvailable($0) ?? false }
-        let aliasAvailable = aliasOutlet.rx.text.orEmpty.asDriver()
-            .map { [weak self] in self?.isAliasAvailable($0) ?? false }
-        
-        aliasAvailable.drive(validationOutlet.rx.isHidden)
-            .addDisposableTo(disposeBag)
-        
-        let hostValid = hostOutlet.rx.text.orEmpty.asDriver()
-            .map { $0.characters.count > 0 }
-        
-        let usernameValid = usernameOutlet.rx.text.orEmpty.asDriver()
-            .map { $0.characters.count > 0 }
-        
-        let passwordValid = passwordOutlet.rx.text.orEmpty.asDriver()
-            .map { $0.characters.count > 0 }
-
-        let isChanged = isAnyFieldChanged()
-        let saveEnabled = Driver.combineLatest(aliasValid,
-                                                              hostValid,
-                                                              usernameValid,
-                                                              passwordValid, isChanged
-        ){ $0 && $1 && $2 && $3 && $4}
-        
-        saveEnabled.drive(saveOutlet.rx.isEnabled).addDisposableTo(disposeBag)
+        viewModel.aliasAvailable.drive(validationOutlet.rx.isHidden).addDisposableTo(disposeBag)
+        viewModel.saveEnabled.drive(saveOutlet.rx.isEnabled).addDisposableTo(disposeBag)
         
         saveOutlet.rx.tap
             .subscribe(onNext: { [unowned self] in
@@ -77,19 +66,6 @@ class HostDetailsViewController: UITableViewController {
             .addDisposableTo(disposeBag)
     }
     
-    private var hostsState : HostsState {
-        return store.observable.value.hostsState
-    }
-    
-    private func populateHostInfo() {
-        guard let alias = hostsState.editingHostAlias else { return }
-        guard let hostInfo = hostsState.allHosts[alias] else { return }
-        aliasOutlet?.text = alias
-        hostOutlet?.text = hostInfo.host
-        usernameOutlet?.text = hostInfo.user
-        passwordOutlet?.text = hostInfo.password
-    }
-    
     private func saveHostAction() -> Action? {
         guard let alias = aliasOutlet.text,
             let host = hostOutlet.text,
@@ -97,8 +73,8 @@ class HostDetailsViewController: UITableViewController {
             let password = passwordOutlet.text else { return nil }
         let newHost = HostInfo(alias: alias, host: host, user: user, password: password)
         
-        if let editingHostAlias = hostsState.editingHostAlias {
-            guard let oldHost = hostsState.allHosts[editingHostAlias] else {
+        if let editingHostAlias = store.hostsState.editingHostAlias {
+            guard let oldHost = store.hostsState.allHosts[editingHostAlias] else {
                 print("host is nil for \(editingHostAlias)")
                 return nil
             }
@@ -108,33 +84,5 @@ class HostDetailsViewController: UITableViewController {
         } else {
             return AddHost(host: newHost)
         }
-    }
-    
-    private func isAliasAvailable(_ alias: String) -> Bool {
-        if alias == hostsState.editingHostAlias { return true }
-        return !hostsState.allHosts.keys.contains(alias)
-    }
-    
-    private func isAnyFieldChanged() -> Driver<Bool> {
-        let oldAlias = aliasOutlet.text
-        let oldHost = hostOutlet.text
-        let oldUsername = usernameOutlet.text
-        let oldPassword = passwordOutlet.text
-        
-        let aliasChanged = aliasOutlet.rx.text.orEmpty.asDriver()
-            .map { $0 != oldAlias }
-        let hostChanged = hostOutlet.rx.text.orEmpty.asDriver()
-            .map { $0 != oldHost }
-        let usernameChanged = usernameOutlet.rx.text.orEmpty.asDriver()
-            .map { $0 != oldUsername }
-        let passwordChanged = passwordOutlet.rx.text.orEmpty.asDriver()
-            .map { $0 != oldPassword }
-        
-        return Driver.combineLatest(aliasChanged,
-                                               hostChanged,
-                                               usernameChanged,
-                                               passwordChanged
-        ){ $0 || $1 || $2 || $3}
-
     }
 }
